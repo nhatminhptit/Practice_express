1b48660792079329a4514560acae5148ae480f39
Minh <nhatminhptit405@gmail.com>
2026-08-13 23:47:52 +0700

    Them tinh nang them cate va lay toan bo cate

---
+++ controllers/category.controller.js
@@ -0,0 +1,52 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma/client.ts";
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export const createCategory = async (req, res) => {
+  try {
+    const { name, status } = req.body;
+
+    const newCategory = await prisma.category.create({
+      data: {
+        name: name,
+        status: status,
+      },
+    });
+
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.log("CHI TIẾT LỖI PRISMA:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getCategories = async (req, res) => {
+  try {
+    const search = req.query.search;
+
+    const categories = await prisma.category.findMany();
+
+    res.json(categories);
+  } catch (error) {
+    console.log("CHI TIẾT LỖI PRISMA:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateCategory = async (req, res) => {
+    try {
+        
+    }
+    catch (error) {
+
+    }
+}
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+import "dotenv/config";
+import categoryRoutes from "./routes/category.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/categories", categoryRoutes);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Đang hoạt đông tại cổng ${port}`);
+  console.log(`Đang hoạt động tại cổng ${port}`);
 });
