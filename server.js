d338ecf5f235d28abbe860a4f57fc8b821bc744d
Minh <nhatminhptit405@gmail.com>
2026-08-13 19:57:42 +0700

    Server chay thu thanh cong

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Đang hoạt đông tại cổng ${port}`);
+});
